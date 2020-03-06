9e9d138bdf11db181e01225e1fdad2f42aa5889f
shinji iida <physicshinzui@gmail.com>
2020-03-06 14:17:12 +0900

    new options were added. normalised hist was applied.

--- draw_sasa_distrib.py
+++ draw_sasa_distrib.py
@@ -6,12 +6,15 @@ import matplotlib.pyplot as plt
 def parser():
     import argparse
     p = argparse.ArgumentParser()
-    p.add_argument('-f', '--sasa_table', required=True, help='.pkl')
+    p.add_argument('-f', '--sasa_table'   , required=True, help='.pkl')
     p.add_argument('-l', '--residue_list' , nargs='*', required=True, help='e.g. PHE66')
+    p.add_argument('-ps', '--plot_style'  , default='plot', help='hist/plot')
+    p.add_argument('-b', '--begin'  , type=int, help='')
+    #p.add_argument('-e', '--end'    , type=int, help='')
     args = p.parse_args()
     return args
 
-def draw(SASA, label, buried_upper_limit=0.1):
+def draw(std_SASA, label, buried_upper_limit=0.2):
     """
     Args:
         std_SASA: [dict] standardized SASA like {PHE105:ASA, ..., TYR200:ASA}
@@ -20,15 +23,15 @@ def draw(SASA, label, buried_upper_limit=0.1):
         Reb: Ratio of exposed probability to buried one. Small value indicates buried states are more dominant than exposed.
     """
     bin_start, bin_end, interval = 0, 1, 0.01
-    hist = np.histogram(SASA, bins=[i for i in np.arange(bin_start,bin_end,interval)])
-    probs, bin_edges = hist[0], hist[1]
+    hist = np.histogram(std_SASA, bins=[i for i in np.arange(bin_start,bin_end,interval)])
+    probs, bin_edges = hist[0]/np.sum(hist[0]), hist[1]
     half_width       = 0.5 * abs(bin_edges[0] - bin_edges[1])
     hist_xpoints     = np.array([edge+half_width for edge in bin_edges[:-1]])
 
     plt.plot(hist_xpoints, probs, label=label)
     plt.axvline(x=buried_upper_limit, c='black')
     plt.xlim(0,1.0)
-    plt.legend()
+    plt.ylim(0,0.8)
 
 def main():
     """
@@ -36,16 +39,33 @@ def main():
         python draw_sasa_distrib.py -f st_dict_sasa.pkl -l PHE66 PHE105
 
     """
-
     args = parser()
-    sasa_table, residues = args.sasa_table, args.residue_list
+    sasa_table, residues, style, begin = args.sasa_table, args.residue_list, args.plot_style, args.begin
     f1 = open(sasa_table,'rb')
     sasa    = pickle.load(f1)
     for residue in residues:
-        draw(sasa[residue], residue)
+        
+        n_points = len(sasa[residue][begin:])
 
+        if style == 'hist':
+            draw(sasa[residue][begin:], label=residue)
+
+        elif style == 'plot':
+            plt.plot(sasa[residue][begin:], label=residue)
+            #plt.xticks(np.arange(0,n_points), 
+            #           np.arange(begin, begin+n_points) 
+            #          )
+            plt.ylim(0,1)
+
+    plt.legend()
     plt.tight_layout()
-    plt.savefig('fig_sasa_distrib.png', dpi=300)
+
+    if style == 'hist':
+        plt.savefig('fig_sasa_hist.png', dpi=300)
+
+    elif style == 'plot':
+        plt.savefig('fig_sasa_series.png' , dpi=300)
+
     #plt.show()
 
 if __name__ == '__main__':
